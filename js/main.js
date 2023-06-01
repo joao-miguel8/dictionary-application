81bbc9cc15bb2a3ef91c73628e03726833c40c04
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-05-31 23:31:09 -0400

    Finished with markup and styling to word-details-information section
    Add: added search input in chosen-word container
    Add: added word-details-information section
    Add: hover style on chosen-word__audio-button

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+// let api = "https://api.dictionaryapi.dev/api/v2/entries/en/pen"
+
+// fetch(api).then( response => response.json()).then((data) => {
+  
+//   // console.log(data);
+//   let wordInfo = (data[0]);
+//   console.log(wordInfo);
+//   const wordDetails =  (wordInfo.meanings[0].definitions);
+//   console.log(wordDetails);
+//   for(let key of wordDetails) {
+//     console.log(key.definition);
+//   }
+// });
