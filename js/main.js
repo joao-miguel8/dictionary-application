5ed57c3c3d7fb5aeee8dcd252259666350fc9f03
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-06-02 22:01:20 -0400

    Add search functionality
    Add: in main.js file added searchResult function
    Add: implemented debounce function with less api request calls
    Refactor: moved search input outside of chosen-word container
    Refactor: added a fixed height for .word-details-information__left-content-card

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
 
+const searchInput = document.querySelector(".search-input");
+const searchedWordEle = document.querySelector(".word-searched");
+const wordAudioButton = document.querySelector("chosen-word__audio-button");
+const phoneticsEle = document.querySelector(".phonetic-text");
 
 
-// let api = "https://api.dictionaryapi.dev/api/v2/entries/en/pen"
 
-// fetch(api).then( response => response.json()).then((data) => {
-  
-//   // console.log(data);
-//   let wordInfo = (data[0]);
-//   console.log(wordInfo);
-//   const wordDetails =  (wordInfo.meanings[0].definitions);
-//   console.log(wordDetails);
-//   for(let key of wordDetails) {
-//     console.log(key.definition);
-//   }
-// });
+// Debounce delay Function for search input reducing the amount of api requests to the server
+function debounce(cbFunc, Delay = 800) {
+  let timeoutCounter;
+  return (...args) => {
+    clearTimeout(timeoutCounter);
+    timeoutCounter = setTimeout(() => cbFunc(...args),Delay);
+  }
+}
+
+// Function api request for search input 
+function searchResult() {
+  let searchValue = searchInput.value;
+  console.log(searchValue);
+  let api = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`;
+  fetch(api).then( response => response.json()).then((data) => {
+    searchedWordEle.textContent = (data[0].word);
+    console.log(data);
+  })
+}
+
+let triggeredDebounce = debounce(() => searchResult());
+
+// <<<<<<<<< EVENT LISTENERS >>>>>>>>
+searchInput.addEventListener("input", triggeredDebounce);
+
+
+// --------------
+// --------------
+  //   let wordInfo = (data[0]);
+  //   console.log(wordInfo);
+  //   const wordDetails =  (wordInfo.meanings[0].definitions);
+  //   console.log(wordDetails);
+  //   for(let key of wordDetails) {
+  //     console.log(key.definition);
+  //   }
+  // })
